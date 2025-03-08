abb17534667784afd3c84163fbdd72192102dca1
HP <solomonmach2004@gmail.com>
2025-03-09 00:00:00 +0300

    fixed Cart Item Model Issues

--- api/models.py
+++ api/models.py
@@ -43,6 +43,7 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    id = models.AutoField(primary_key=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
